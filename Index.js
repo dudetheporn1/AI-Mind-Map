2e8b4c8bdb09aa288852cc06bb4a452daedc3a59
dudetheporn1 <dudetheporn1@gmail.com>
2025-05-14 09:20:39 -0700

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import MindMapApp from './MindMapApp';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => MindMapApp);
